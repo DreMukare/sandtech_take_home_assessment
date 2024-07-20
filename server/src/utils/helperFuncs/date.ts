7f09c9b0fdedcfb5455048c29e772b5c1c7c68bd
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-20 04:04:27 +0300

    feat: wrote helper functions to manipulate date

---
+++ server/src/utils/helperFuncs/date.ts
@@ -0,0 +1,36 @@
+function numberOfDaysInMonth(year: number, month: number) {
+  if (month === 2) {
+    return year % 4 === 0 ? 29 : 28;
+  } else if ([4, 6, 9, 11].includes(month)) {
+    return 30;
+  } else {
+    return 31;
+  }
+}
+
+export function getSevenDaysFromDay(day: number, month: number, year: number) {
+  const daysInMonth = numberOfDaysInMonth(year, month);
+  const sevenDays = [];
+
+  for (let i = 0; i < 7; i++) {
+    if (day + i <= daysInMonth) {
+      sevenDays.push(day + i);
+    } else {
+      sevenDays.push(day + i - daysInMonth);
+    }
+  }
+
+  return sevenDays;
+}
+
+export function getDayMonthAndYearFromDate(date: string) {
+  const day = Number(date.split("-")[2]);
+  const month = Number(date.split("-")[1]);
+  const year = Number(date.split("-")[0]);
+
+  return {
+    day,
+    month,
+    year,
+  };
+}
