acf0303633ef9d9b4800ad7bd418b1851a4b266a
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-20 04:05:17 +0300

    feat: exposing weather api key from .env

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,6 +5,8 @@ dotenv.config();
 export const DB_CONN_STRING = process.env.DB_CONN_STRING || "";
 export const DB_NAME = process.env.DB_NAME || "";
 
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY || "";
+
 export const mongo = {
   DB_CONN_STRING,
   DB_NAME,
