ce1d159d5857ed15cbcb3973ffdcfb401f9bd1f2
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-22 20:49:20 +0300

    feat: wrote type for api response

---
+++ client/src/utils/types/weather.ts
@@ -0,0 +1,12 @@
+export interface WeatherForecast {
+  _id: string; //"669d85af0f81b111f58163c0",
+  date: string; //"2024-07-28",
+  maxTempC: number; //25.5,
+  minTempC: number; //13.8,
+  willItRain: number; //false,
+  sunrise: string; //"06:37 AM",
+  sunset: string; //"06:41 PM",
+  condition: string; //"Sunny",
+  conditionIcon: string; //"https//cdn.weatherapi.com/weather/64x64/day/113.png",
+  __v: number; //0
+}
