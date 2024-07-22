3022bd9081f71f39b6ff87f062c7746c698f5742
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-22 20:56:41 +0300

    chore: installed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import { initExternalServices } from "./handlers/init-external-services";
 import routes from "./routes/routes";
-import weatherService from "./services/weather.service";
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use("/api/v1", routes);
 
