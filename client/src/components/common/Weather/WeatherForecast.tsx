402bf6e991dee28d2ac986c009cd5a3253b2dc98
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-22 21:02:17 +0300

    minor: displaying weather forecast data

--- client/src/components/common/Weather/WeatherForecast.tsx
+++ client/src/components/common/Weather/WeatherForecast.tsx
@@ -1,16 +1,41 @@
 import { useEffect, useState } from "react";
+import { getCurrentDateAsString } from "../../../utils/funcs/date";
 
 const WeatherForecast = () => {
   const [forecastData, setForecastData] = useState<any>(null);
-  const dateString = useEffect(() => {
+  const dateString = getCurrentDateAsString();
+
+  useEffect(() => {
     const getForecastData = async () => {
       const response = await fetch(
         `http://localhost:3000/api/v1/weather?date=${dateString}`
       );
+      const data = await response.json();
+      setForecastData(data);
     };
-  }, [forecastData]);
 
-  return <div>WeatherForecast</div>;
+    getForecastData();
+  }, []);
+
+  if (!forecastData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {forecastData.map((forecast: any) => (
+        <div key={forecast._id}>
+          <h3>{forecast.date}</h3>
+          <p>{forecast.condition}</p>
+          <img src={forecast.conditionIcon} alt={forecast.condition} />
+          <p>Max Temp: {forecast.maxTempC}°C</p>
+          <p>Min Temp: {forecast.minTempC}°C</p>
+          <p>Sunrise: {forecast.sunrise}</p>
+          <p>Sunset: {forecast.sunset}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default WeatherForecast;
