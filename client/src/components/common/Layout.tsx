fab3eddb22cbd263a55b3613f26b4a6a7c82b0cf
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-24 22:06:25 +0300

    feat: created layout component for pages

--- client/src/components/common/Layout.tsx
+++ client/src/components/common/Layout.tsx
@@ -1,5 +1,20 @@
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = (props: { children: React.ReactNode }) => {
+  const { children } = props;
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        height: "80vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
