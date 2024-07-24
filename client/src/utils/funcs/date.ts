1553a3f6169bca5923da2da9c1cd8b3bc7041adf
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-07-25 01:00:30 +0300

    feat: wrote utility function to get friendly date string from date

--- client/src/utils/funcs/date.ts
+++ client/src/utils/funcs/date.ts
@@ -8,3 +8,30 @@ export function getCurrentDateAsString(): string {
   const formattedDate = `${year}-${month}-${day}`;
   return formattedDate;
 }
+
+export function getFriendlyDateString(date: string): string {
+  const dateArray = date.split("-");
+  const month = Number(dateArray[1]);
+  const day = Number(dateArray[2]);
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dayOfWeek = days[new Date(date).getDay()];
+  const monthName = months[month - 1];
+
+  return `${dayOfWeek}, ${monthName} ${day}`;
+}
